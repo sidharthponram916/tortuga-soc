e0cbbd863c1648ab20305d4e10e413fe8f4a5415
Sidharth Ponram <sidponram@gmail.com>
2025-02-23 16:15:55 -0500

    add google analytics support

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -4,8 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
+  css: [
+    "~/assets/css/main.css",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
+  gtag: {
+    id: "G-2SNMH20JBD",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
+
+  modules: ["nuxt-gtag"],
 });
