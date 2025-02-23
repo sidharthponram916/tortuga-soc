93f69106a3e63efe03f4d76a6b29e4b52d4e3520
Sidharth Ponram <sidponram@gmail.com>
2025-02-23 00:51:42 -0500

    fix no comma

--- src/app.vue
+++ src/app.vue
@@ -18,7 +18,7 @@ useHead({
   title: "Tortuga SOC",
   meta: [
     { property: "og:title", content: "Tortuga" },
-    { proprty: "og:description", content: "Make the most out of the Schedule of Classes."}
+    { proprty: "og:description", content: "Make the most out of the Schedule of Classes."},
     { property: "og:type", content: "website" },
     { property: "og:url", content: "https://tortuga-soc.vercel.app/" },
     { property: "og:image", content: "https://tortuga-soc.vercel.app/assets/images/preview.png" },
